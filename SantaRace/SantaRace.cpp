c6f5eb95b953a2c35697cce8ef96d1fb04ce71ee
dbwls231 <mina45097128@gmail.com>
2022-03-13 19:26:03 +0900

    final
    제출본

--- SantaRace/SantaRace.cpp
+++ SantaRace/SantaRace.cpp
@@ -1,36 +1,126 @@
 ﻿#include <bangtal.h>
+#include <string>
+using namespace std;
 
 SceneID scene;
 
-ObjectID startButton, endButton, playButton, santa, present;
+ObjectID startButton, endButton, santa;
+ObjectID arrowUI[5], presents[20];
 
-bool presentDrop = false;
+int santaX = 0, santaY = 550;
+int presentScore=0, arrowNum=0;
+int arrow[5];
 
-int santaX = 0, santaY = 500;
+TimerID presentTimer,deliveryTimer;
 
-TimerID timer1;
+bool isPlaying=false, presentDirect=true;
 
+//화살표 이미지 변경
+void changeArrowUi(int i, int arrow) {
+	arrowNum = 0;
+	switch (arrow)
+	{
+	case 1:
+		setObjectImage(arrowUI[i], "Images/up.png");
+		break;
+	case 2:
+		setObjectImage(arrowUI[i], "Images/right.png");
+		break;
+	case 3:
+		setObjectImage(arrowUI[i], "Images/down.png");
+		break;
+	case 4:
+		setObjectImage(arrowUI[i], "Images/left.png");
+	}
+	showObject(arrowUI[i]);
+}
+
+//화살표 초기화
+void changeArrow() {
+	printf("\n");
+	for (int i = 0; i < 5; i++) {
+		arrow[i] = rand() % 4 + 1;
+		changeArrowUi(i, arrow[i]);
+		printf("%d", arrow[i]);
+	}
+}
+
+//입력한 키와 화살표가 맞는지
+bool checkArrow(KeyCode code) {
+	printf("\narrowNum:%d, num:%d, KeyCode:", arrowNum, arrow[arrowNum]);
+	switch (code)
+	{
+	case KeyCode::KEY_UP_ARROW:
+		printf("1");
+		if (arrow[arrowNum] == 1) {
+			return true;
+		}
+		break;
+	case KeyCode::KEY_RIGHT_ARROW:
+		printf("2");
+		if (arrow[arrowNum] == 2) {
+			return true;
+		}
+		break;
+	case KeyCode::KEY_DOWN_ARROW:
+		printf("3");
+		if (arrow[arrowNum] == 3) {
+			return true;
+		}
+		break;
+	case KeyCode::KEY_LEFT_ARROW:
+		printf("4");
+		if (arrow[arrowNum] == 4) {
+			return true;
+		}
+		break;
+	}
+	return false;
+	
+}
+
+//선물 개수 관리
+void presentMg(int num) {
+	//맞추면 하나 추가
+	if (num > 0) { 
+		showObject(presents[presentScore]);
+		presentScore += num;
+		santaX += 60;
+	}
+	//틀리면 하나 마이너스
+	else { 
+		if (presentScore > 0) {
+			presentScore += num;
+			hideObject(presents[presentScore]);
+			santaX -= 60;
+		}
+	}
+	locateObject(santa, scene, santaX, santaY);
+}
 
 void startGame() {
 	hideObject(startButton);
 	hideObject(endButton);
 
-	showObject(playButton);
+	presentScore = 0;
 
-	setTimer(timer1, 10.f);
-	startTimer(timer1);
+	setTimer(presentTimer, 20.f);
+	startTimer(presentTimer);
 }
 
-void endGame(bool succeess) {
-	if (succeess) {
-		showMessage("선물 배달 성공~");
-	}
-	else {
-		showMessage("선물 배달 실패!!");
-	}
+void finishGame() {
+	isPlaying = false;
+	string str = to_string(presentScore);
+	str += "개\n선물 배달 성공~";
+	const char* cstr = str.c_str();
+	showMessage(cstr);
 
-	hideObject(playButton);
-
+	for (int i = 0; i < 5; i++) {
+		hideObject(arrowUI[i]);
+	}
+	for (int i = 0; i < presentScore; i++) {
+		hideObject(presents[i]);
+	}
 	setObjectImage(startButton, "Images/restart.png");
 	showObject(startButton);
 	showObject(endButton);
@@ -38,7 +128,26 @@ void endGame(bool succeess) {
 	santaX = 0;
 	locateObject(santa, scene, santaX, santaY);
 
-	stopTimer(timer1);
+	stopTimer(presentTimer);
+}
+
+void keyboardCallback(KeyCode code, KeyState state) {
+	
+	if (state == KeyState::KEY_PRESSED&&isPlaying) {
+		if (checkArrow(code)) {
+			printf("correct");
+			arrowNum++;
+			if (arrowNum > 4) {
+				presentMg(1);
+				changeArrow();
+			}
+		}
+		else {
+			printf("false");
+			presentMg(-1);
+			changeArrow();
+		}
+	}
 }
 
 void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
@@ -46,24 +155,16 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 		endGame();
 	}
 	else if (object == startButton) {
+		isPlaying = true;
+		changeArrow();
 		startGame();
-	}
-	else if (object == playButton) {
-		santaX = santaX + 30;
-		locateObject(santa, scene, santaX, santaY);
-		locateObject(present, scene, santaX + 30, santaY + 10);
-
-		if (santaX > 1280) {
-			endGame(true);
-		}
 	}
 }
 
 void timerCallback(TimerID timer) {
-	if (timer == timer1)
-		endGame(false);
+	if (timer == presentTimer)
+		finishGame();
 }
-
 
 int main() {
 	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
@@ -72,6 +173,7 @@ int main() {
 
 	setMouseCallback(mouseCallback);
 	setTimerCallback(timerCallback);
+	setKeyboardCallback(keyboardCallback);
 
 	scene = createScene("Santa Race", "Images/background.png");
 
@@ -87,16 +189,21 @@ int main() {
 	locateObject(santa, scene, santaX, santaY);
 	showObject(santa);
 
-	present = createObject("Images/present.png");
-	locateObject(present, scene, santaX + 30, santaY + 10);
-	scaleObject(present, 0.05f);
-	showObject(present);
 
-	playButton = createObject("Images/play.png");
-	locateObject(playButton, scene, 610, 30);
+	for (int i = 0; i < 5; i++) {
+		arrowUI[i] = createObject("Images/right.png");
+		locateObject(arrowUI[i], scene, 460 + i * 50, 30);
+	}
 
-	timer1 = createTimer(10.f);
-	showTimer(timer1);
+	for (int i = 0; i < 20; i++) {
+		presents[i] = createObject("Images/present.png");
+		scaleObject(presents[i],0.1f);
+		locateObject(presents[i], scene, 0 + i * 60, 560);
+	}
+	presentTimer = createTimer(20.f);
+
+	showTimer(presentTimer);
 
 	startGame(scene);
+
 }
